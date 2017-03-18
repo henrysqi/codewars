0e0beb70cf80a6d520e270bb62d53fa3f0aba400
Henry Qi <henryqi222@gmail.com>
2017-03-17 21:07:10 -0700

    still fail

--- 5/x5-numbersthatareapoweroftheirsumofdigits.js
+++ 5/x5-numbersthatareapoweroftheirsumofdigits.js
@@ -1,26 +1,29 @@
 function powerSumDigTerm(n) {
 	let satisfies = [];
-	
-	let i = 10;
+	let current = 81;
 	while (satisfies.length < n) {
-		let sum = sumOfDigs(i);
-		let j = 2
-		while (Math.pow(sum, j) < i) {
-			if (Math.pow(sum, j) === i) {
-				satisfies.push(i)
-			}
-			j++;
+		if (squareOfDigs(current)){
+			satisfies.push(current);
 		}
-		i++;
 	}
-	return satisfies.pop();
 }
 
-function sumOfDigs(num) {
+function squareOfDigs(num) {
 	let numstring = num.toString();
 	let result = 0;
-	for (let i = 0; i < numstring.length; i++) {
+	let powerResult = 0;
+	let counter = 0;
+
+	for (let i = 0; i < numstring.length; i++){
 		result += Number(numstring[i]);
 	}
-	return result;
+	while (powerResult < num) {
+		counter++;
+		if (powerResult === num) {
+			return true;
+		} else {
+			powerResult = Math.pow(result, counter);
+		}
+	}
+	return false;
 }

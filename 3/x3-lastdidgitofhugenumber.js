7e68b0fdcc645833aa9dc8ebd8e1cda07db47169
Henry Qi <henryqi222@gmail.com>
2018-02-10 01:40:18 -0800

    3, alg not yet confirmed to work yet

--- 3/x3-lastdidgitofhugenumber.js
+++ 3/x3-lastdidgitofhugenumber.js
@@ -1,15 +1,41 @@
+/*
+1. as[1]^X mod 10
+2. find how many cycles of base
+3. next num in array to the X mod result from (2)
+4. do it till get lastnum mod Y, unless stopped earlier (every num in cycle is even and mod is 2 => 0)
+5. evaluate and go backwards
+
+*/
+
 function lastDigit(as){
-  if (as.length === 0){
-    return 1;
-  }
-  if (as.length === 1){
-    return as[0]%10;
-  }
-  var lastNum = as[as.length-1] === 0 ? 1 : Math.pow(as[as.length-2], as[as.length-1])
-  
-  var newAs = as.slice(0,-2);
-  newAs.push(lastNum);
-  
-  return lastDigit(newAs)
-  
+
+  return getBigNum(as, 10);
 }
+
+function getBigNum(arr, mod) {
+  console.log(arr, mod)
+  if (arr.length === 1) {
+    return arr[0]%mod;
+  } else {
+    var lastDigitsSeen = [];
+    var i = 1;
+    var base = arr.shift();
+    while (true) {
+      var lastDigit = getLastDigit(Math.pow(base, i));
+      if (lastDigitsSeen.indexOf(lastDigit) === -1) {
+        lastDigitsSeen.push(lastDigit)
+      } else {
+        break;
+      }
+    }
+    var bigNum = getBigNum(arr, lastDigitsSeen.length);
+    return (Math.pow(base, bigNum))%mod;
+  }
+}
+
+function getLastDigit(num) {
+  var str = num.toString();
+  return str[str.length-1];
+}
+
+lastDigit([12,30,21]);
